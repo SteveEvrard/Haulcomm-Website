3a7c7ddbde63aeebc4ded42861a98d5869b48529
Steve Evrard <stevelevrard@gmail.com>
2024-04-10 10:21:32 -0400

    adjust sections

--- src/components/services.jsx
+++ src/components/services.jsx
@@ -2,14 +2,10 @@ import React from "react";
 
 export const Services = (props) => {
   return (
-    <div id="services" className="text-center">
+    <div id="brokers-owners" className="text-center">
       <div className="container">
         <div className="section-title">
-          <h2>Our Services</h2>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit duis sed
-            dapibus leonec.
-          </p>
+          <h2>Brokers and Owners</h2>
         </div>
         <div className="row">
           {props.data
